02d75a9bb567aefbd82f73a349743bdfabd7a336
S.R.U <sandorodri1@gmail.com>
2022-06-17 11:24:59 -0700

    test 55

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                   Stanky!
+                   Hola ulises!
                 </div>
 
                 <div class="links">
